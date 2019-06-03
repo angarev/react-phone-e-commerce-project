cb609ec9e049f8bc1026ef68a1f8951580a746b5
Panayot Angarev <pancho.angarev@gmail.com>
2019-06-03 21:10:27 +0300

    Add buttons to the cart page

--- src/components/Cart/CartColumns.js
+++ src/components/Cart/CartColumns.js
@@ -2,24 +2,24 @@ import React from 'react';
 
 export default function CartColumns() {
     return (
-        <div className="container-fluid text-center my-5">
-            <div className="row">
-                <div className="col">
+        <div className="container-fluid">
+            <div className="row text-center my-5">
+                <div className="col-10 mx-auto col-lg-2">
                     <p className="text-uppercase">products</p>
                 </div>
-                <div className="col">
+                <div className="col-10 mx-auto col-lg-2">
                     <p className="text-uppercase">name of products</p>
                 </div>
-                <div className="col">
+                <div className="col-10 mx-auto col-lg-2">
                     <p className="text-uppercase">Price</p>
                 </div>
-                <div className="col">
+                <div className="col-10 mx-auto col-lg-2">
                     <p className="text-uppercase">quantity</p>
                 </div>
-                <div className="col">
+                <div className="col-10 mx-auto col-lg-2">
                     <p className="text-uppercase">remove</p>
                 </div>
-                <div className="col">
+                <div className="col-10 mx-auto col-lg-2">
                     <p className="text-uppercase">total</p>
                 </div>
             </div>
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 export default function CartItem({item, value}) {
     
@@ -20,8 +20,21 @@ export default function CartItem({item, value}) {
             <div className="col-10 mx-auto col-lg-2">
                 <span className="d-lg-none">Price: </span>${price}
             </div>
-            
-            <h1>CartItem</h1>
+            <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+                <div className="d-flex justify-content-center">
+                    <div>
+                        <button className={`btn btn-outline-info mx-1`} onClick={() => decrement(id)}>&#8722;</button>
+                        <span className={`btn btn-outline-info mx-1`}>{count}</span>
+                        <button className={`btn btn-outline-info mx-1`} onClick={() => increment(id)}>&#43;</button>
+                    </div>
+                </div>
+            </div>
+            <div className="col-10 mx-auto col-lg-2">
+                <strong><i class="fas fa-trash-alt"></i> </strong>
+            </div>
+            <div className="col-10 mx-auto col-lg-2">
+                <strong>item total: </strong> ${total}
+            </div>
         </div>
     )
 }
