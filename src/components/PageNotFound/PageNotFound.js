1284d46336746d4de90f061ff17bb48e31414b9c
Panayot Angarev <pancho.angarev@gmail.com>
2019-06-10 07:35:24 +0300

    Adding page not found

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Title from '../Title/Title';
+import Breadcrumbs from '../Breadcrumbs/Breadcrumbs';
+
+
 
 export default class PageNotFound extends Component {
     render() {
+
+        const crumbs = [
+            {title: 'Home', path: '/'}, 
+            {title: 'Shop', path: '/'},
+            {title: 'Page not found'}
+        ];
+
         return (
-            <div>
-                <h3>PageNotFound</h3>
-            </div>
+            <React.Fragment>
+                <Breadcrumbs crumbs = {crumbs} title = ""/>
+                <div className="container my-5">
+                    <div className="row">
+                        <div className="col align-self-center text-center">
+                            <Title  title="404"/>
+                            <h1 className="py-3">Error</h1>
+                            <h2 className="py-3">Page not found</h2>
+                            <h3 className="py-3">The requested URL <span className="text-danger">{this.props.location.pathname}</span>{" "} was not found </h3>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
